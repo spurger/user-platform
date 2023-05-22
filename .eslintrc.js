66979c259b9b7f59f2a6fe724e3f7ab4ba9d206f
Spurger <spurger93@gmail.com>
2023-05-22 23:59:01 +0200

    add eslint + prettier support

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+    },
+    extends: ["plugin:vue/vue3-recommended", "prettier"],
+    plugins: ["prettier"],
+    rules: {},
+}
